eb4c055296ef793baf693cd358b6074ebd9c104b
kaisermann <kaisermann1890@gmail.com>
2024-06-18 18:14:46 -0400

    se crea la clase de enfermedad

---
+++ enfermedad.py
@@ -0,0 +1,6 @@
+class Enfermedad:
+    def __init__(self, infeccion_probable, promedio_pasos):
+        self.infeccion_probable = infeccion_probable
+        self.promedio_pasos = promedio_pasos
+        self.enfermo = False
+        self.contador = 0
