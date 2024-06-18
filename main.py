7bd3fdfdbc44b8dcf8dadec527188e0ada2490f5
kaisermann <kaisermann1890@gmail.com>
2024-06-18 18:21:01 -0400

    Se crea el archivo main muy basico para empezar el proyecto

---
+++ main.py
@@ -0,0 +1,12 @@
+from enfermedad import Enfermedad
+from comunidad import Comunidad
+from simulador import Simulador
+from generar_datos import generar_nombres
+
+
+def main():
+    generar_nombres()
+
+
+if __name__ == "__main__":
+    main()
