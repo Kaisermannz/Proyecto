a7d60679b79d5fb3dba323f96ceeb9ff53e76e08
kaisermann <kaisermann1890@gmail.com>
2024-06-24 20:01:59 -0400

    Se cambio la clase haciendola privada

--- comunidad.py
+++ comunidad.py
@@ -7,12 +7,48 @@ class Comunidad:
         num_infectados,
         probabilidad_conexion_fisica,
     ):
-        self.num_ciudadanos = num_ciudadanos
-        self.promedio_conexion_fisica = promedio_conexion_fisica
-        self.enfermedad = enfermedad
-        self.num_infectados = num_infectados
-        self.probabilidad_conexion_fisica = probabilidad_conexion_fisica
-        self.ciudadanos = []
+        self.__num_ciudadanos = num_ciudadanos
+        self.__promedio_conexion_fisica = promedio_conexion_fisica
+        self.__enfermedad = enfermedad
+        self.__num_infectados = num_infectados
+        self.__probabilidad_conexion_fisica = probabilidad_conexion_fisica
+        self.__ciudadanos = []
 
+    # Getters:
+    def get_num_ciudadanos(self):
+        return self.__num_ciudadanos
+
+    def get_promedio_conexion_fisica(self):
+        return self.__promedio_conexion_fisica
+
+    def get_enfermedad(self):
+        return self.__enfermedad
+
+    def get_num_infectados(self):
+        return self.__num_infectados
+
+    def get_probabilidad_conexion_fisica(self):
+        return self.__probabilidad_conexion_fisica
+
+    def get_ciudadanos(self):
+        return self.__ciudadanos
+
+    # Setters
+    def set_num_ciudadanos(self, num_ciudadanos):
+        self.__num_ciudadanos = num_ciudadanos
+
+    def set_promedio_conexion_fisica(self, promedio_conexion_fisica):
+        self.__promedio_conexion_fisica = promedio_conexion_fisica
+
+    def set_enfermedad(self, enfermedad):
+        self.__enfermedad = enfermedad
+
+    def set_num_infectados(self, num_infectados):
+        self.__num_infectados = num_infectados
+
+    def set_probabilidad_conexion_fisica(self, probabilidad_conexion_fisica):
+        self.__probabilidad_conexion_fisica = probabilidad_conexion_fisica
+
+    # Método para agregar ciudadano
     def agregar_ciudadano(self, ciudadano):
-        self.ciudadanos.append(ciudadano)
+        self.__ciudadanos.append(ciudadano)
