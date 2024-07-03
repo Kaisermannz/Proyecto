16762b48e7b9c864c8f44e68639c6683232ab3a7
kaisermann <kaisermann1890@gmail.com>
2024-07-02 22:26:45 -0400

    se agregaron algunos metodos y se modificaron algunos

--- ciudadano.py
+++ ciudadano.py
@@ -1,6 +1,5 @@
 from enfermedad import Enfermedad
 
-
 class Ciudadano:
     def __init__(self, id, nombre, apellido, comunidad, enfermedad=None):
         self.__id = id
@@ -8,9 +7,8 @@ class Ciudadano:
         self.__apellido = apellido
         self.__comunidad = comunidad
         self.__enfermedad = enfermedad
-        self.__estado = (
-            False if enfermedad else True
-        )  # True para sano False para enfermo/muerto
+        self.__estado = True if enfermedad is None else False
+        self.__dias_enfermo = 0
 
     def set_id(self, id):
         if isinstance(id, int):
@@ -27,7 +25,7 @@ class Ciudadano:
         return self.__nombre
 
     def set_apellido(self, apellido):
-        if isinstance(apellido, int):
+        if isinstance(apellido, str):
             self.__apellido = apellido
 
     def get_apellido(self):
@@ -43,6 +41,8 @@ class Ciudadano:
     def set_enfermedad(self, enfermedad):
         if isinstance(enfermedad, Enfermedad):
             self.__enfermedad = enfermedad
+            self.__estado = False
+            self.__dias_enfermo = 0
 
     def get_enfermedad(self):
         return self.__enfermedad
@@ -55,13 +55,23 @@ class Ciudadano:
 
     def get_enfermo(self):
         return not self.__estado
-
+        
     def set_enfermo(self, estado):
         self.__estado = not estado
-        if estado and self.__enfermedad is None:
-            self.__enfermedad = Enfermedad(
-                infeccion_probable=0.1, promedio_pasos=5
-            )  # Ejemplo de valores
-        elif not estado:
+        if estado:
+            if self.__enfermedad is None:
+                self.__enfermedad = Enfermedad(infeccion_probable=0.1, promedio_pasos=5)
+            self.__dias_enfermo += 1
+        else:
             self.__enfermedad = None
+            self.__dias_enfermo = 0
 
+    def get_dias_enfermo(self):
+        return self.__dias_enfermo
+
+    def __str__(self):
+        return f"Ciudadano(id={self.__id}, nombre={self.__nombre}, apellido={self.__apellido}, enfermo={self.get_enfermo()}, dias_enfermo={self.__dias_enfermo})"
+
+    def incrementar_dias_enfermo(self):
+        if self.get_enfermo():
+            self.__dias_enfermo += 1
