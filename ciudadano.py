8089f24b4e93dcf1fdfcde794bfb4b708fdd780f
kaisermann <kaisermann1890@gmail.com>
2024-07-08 18:30:57 -0400

    se cambiaron algunos metodos

--- ciudadano.py
+++ ciudadano.py
@@ -7,71 +7,54 @@ class Ciudadano:
         self.__apellido = apellido
         self.__comunidad = comunidad
         self.__enfermedad = enfermedad
-        self.__estado = True if enfermedad is None else False
+        self.__estado = "susceptible"
         self.__dias_enfermo = 0
-
-    def set_id(self, id):
-        if isinstance(id, int):
-            self.__id = id
 
     def get_id(self):
         return self.__id
 
-    def set_nombre(self, nombre):
-        if isinstance(nombre, str):
-            self.__nombre = nombre
-
     def get_nombre(self):
         return self.__nombre
-
-    def set_apellido(self, apellido):
-        if isinstance(apellido, str):
-            self.__apellido = apellido
 
     def get_apellido(self):
         return self.__apellido
 
-    def set_comunidad(self, comunidad):
-        if isinstance(comunidad, str):
-            self.__comunidad = comunidad
-
     def get_comunidad(self):
         return self.__comunidad
-
-    def set_enfermedad(self, enfermedad):
-        if isinstance(enfermedad, Enfermedad):
-            self.__enfermedad = enfermedad
-            self.__estado = False
-            self.__dias_enfermo = 0
 
     def get_enfermedad(self):
         return self.__enfermedad
 
-    def set_estado(self, estado):
-        self.__estado = estado
-
     def get_estado(self):
         return self.__estado
 
+    def set_estado(self, estado):
+        if estado in ["susceptible", "infectado", "recuperado"]:
+            if estado == "infectado" and self.__estado == "susceptible":
+                self.__estado = estado
+            elif estado == "recuperado" and self.__estado == "infectado":
+                self.__estado = estado
+                self.__enfermedad = None
+            elif estado == "susceptible" and self.__estado != "recuperado":
+                self.__estado = estado
+                self.__enfermedad = None
+                self.__dias_enfermo = 0
+
     def get_enfermo(self):
-        return not self.__estado
+        return self.__estado == "infectado"
         
     def set_enfermo(self, estado):
-        self.__estado = not estado
         if estado:
-            if self.__enfermedad is None:
-                self.__enfermedad = Enfermedad(infeccion_probable=0.1, promedio_pasos=5)
-            self.__dias_enfermo += 1
+            self.set_estado("infectado")
         else:
-            self.__enfermedad = None
-            self.__dias_enfermo = 0
+            self.set_estado("recuperado")
 
     def get_dias_enfermo(self):
         return self.__dias_enfermo
 
-    def __str__(self):
-        return f"Ciudadano(id={self.__id}, nombre={self.__nombre}, apellido={self.__apellido}, enfermo={self.get_enfermo()}, dias_enfermo={self.__dias_enfermo})"
-
     def incrementar_dias_enfermo(self):
-        if self.get_enfermo():
+        if self.__estado == "infectado":
             self.__dias_enfermo += 1
+
+    def __str__(self):
+        return f"Ciudadano(id={self.__id}, nombre={self.__nombre}, apellido={self.__apellido}, estado={self.__estado}, dias_enfermo={self.__dias_enfermo})"
